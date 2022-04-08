1cfc3c60eca3b349e643825a1dd1a3a4fa09be35
cornelious <cornelioust@dipar.co.za>
2022-04-08 17:53:22 +0200

    implemented UI login logic

---
+++ RetailManagerDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using RetailManagerDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace RetailManagerDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
