a109f2a75c08ca420351a9fc2d2949633cf2cc7a
cornelious <cornelioust@dipar.co.za>
2022-06-07 22:34:31 +0200

    #11 implement Get User Data

---
+++ DataManagr/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity;
+using RetailManagerDataManager.Library.DataAccess;
+using RetailManagerDataManager.Library.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace DataManagr.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+       
+        public List<UserModel> GetById()
+        {
+            UserData data = new UserData();
+
+            var id = RequestContext.Principal.Identity.GetUserId();
+
+            var collection = data.GetUserById(id);   
+
+            return collection;
+        }
+    }
+}
