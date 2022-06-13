305cd7670debc878261500bd63faa75049b1d7c9
cjTarwireyi <codzatazz@gmail.com>
2022-06-13 21:45:58 +0200

    #14 Adding items to cart

---
+++ RetailManagerDesktopUI.Library/Models/CartItemModel.cs
@@ -0,0 +1,17 @@
+﻿namespace RetailManagerDesktopUI.Library.Models
+{
+    public class CartItemModel
+    {
+        public ProductModel Product { get; set; }
+        public int QuantityInCart { get; set; }      
+
+        public string DisplayText
+        {
+            get 
+            {
+                return $"{Product.ProductName.TrimEnd()}({QuantityInCart})"; 
+            }
+        }
+
+    }
+}
